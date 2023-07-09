a709af6b18b29cfab0b3ade22e8264fd94e1e107
nicolashenrique921 <nicolashenrique921@gmail.com>
2023-07-09 19:36:32 -0300

    valor_de_PI

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+		
+	
+        System.out.println(VALOR_DE_PI);
     }
 }
