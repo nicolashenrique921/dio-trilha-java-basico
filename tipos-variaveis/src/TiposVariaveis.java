fbf1258f3cb85c62f723fd503e1431e37a8972d7
nicolashenrique921 <nicolashenrique921@gmail.com>
2023-07-09 19:40:42 -0300

    Declaracao_variaveis_constates

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,12 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
 
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+        
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
 
+        /*
+		 * ao usar a palavra reservada final, você determina que jamais
+		 * esta variavel poderá obter outro valor;
+		 * logo a linha 25 vai apresentar um erro de compilação
+		 * isso é considerado uma CONSTANTE na linguagem Java
+		 */
+
         final double VALOR_DE_PI = 3.14;
-		
+		//VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
 	
         System.out.println(VALOR_DE_PI);
     }
