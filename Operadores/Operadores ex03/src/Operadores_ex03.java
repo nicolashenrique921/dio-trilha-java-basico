234a171d7183c397308731502708516342895848
nicolashenrique921 <nicolashenrique921@gmail.com>
2023-08-13 18:04:40 -0300

    Operadors ex03 dio

---
+++ Operadores/Operadores ex03/src/Operadores_ex03.java
@@ -0,0 +1,20 @@
+public class Operadores_ex03 {
+    public static void main(String[] args) throws Exception {
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
