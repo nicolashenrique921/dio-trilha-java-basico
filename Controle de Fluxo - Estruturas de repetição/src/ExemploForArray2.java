9611017e298e01f8362afc07babb58d8418706e2
Nicolas <nicolashenrique921@gmail.com>
2023-12-12 10:23:57 -0300

    Estruturas de Repetição

---
+++ Controle de Fluxo - Estruturas de repetição/src/ExemploForArray2.java
@@ -0,0 +1,11 @@
+public class ExemploForArray2 {
+public static void main(String[] args) {
+	String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+	
+        //Forma abreviada
+	for(String aluno : alunos) {
+	  System.out.println(aluno);
+	}
+
+}
+}
